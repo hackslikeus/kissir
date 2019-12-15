89000e01fd635c0ce5653e4e7d2375e25042fc46
gary kissell <garykissell@yahoo.com>
2019-12-15 10:01:57 -0600

    Update pytiful.py
    added some useful comments and edited unusefuls

--- pytiful.py
+++ pytiful.py
@@ -12,33 +12,33 @@ print("press a button!") #used for testing
 
 #Define our button selection- 5 buttons for station choices:
 
-#First Button and url
+#First Button 
 button1 = digitalio.DigitalInOut(board.D24)
 button1.direction = digitalio.Direction.INPUT
 button1.pull = digitalio.Pull.UP
 
-#Second Button and url
+#Second Button
 button2 = digitalio.DigitalInOut(board.D23)
 button2.direction = digitalio.Direction.INPUT
 button2.pull = digitalio.Pull.UP
 
-#Third Button and url
+#Third Button 
 button3 = digitalio.DigitalInOut(board.D5)
 button3.direction = digitalio.Direction.INPUT
 button3.pull = digitalio.Pull.UP
 
-#Fourth Button and url
+#Fourth Button
 button4 = digitalio.DigitalInOut(board.D17)
 button4.direction = digitalio.Direction.INPUT
 button4.pull = digitalio.Pull.UP
 
-#Fifth button and url
+#Fifth button
 button5 = digitalio.DigitalInOut(board.D22)
 button5.direction = digitalio.Direction.INPUT
 button5.pull = digitalio.Pull.UP
 
 #A poor mans pause button in case of awful sound
-button6 = digitalio.DigitalInOut(board.D7)
+button6 = digitalio.DigitalInOut(board.D27)
 button6.direction = digitalio.Direction.INPUT
 button6.pull = digitalio.Pull.UP
 
