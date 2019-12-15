1062b23dd51e83233ad46b69d7578f08bee1cd80
gary kissell <garykissell@yahoo.com>
2019-12-15 09:26:13 -0600

    Update pytiful.py

--- pytiful.py
+++ pytiful.py
@@ -8,39 +8,47 @@ import os
 import board
 import digitalio
 
-print("press a button!")
+print("press a button!") #used for testing
 
+#Define our button selection- 5 buttons for station choices:
+
+#First Button and url
 button1 = digitalio.DigitalInOut(board.D24)
 button1.direction = digitalio.Direction.INPUT
 button1.pull = digitalio.Pull.UP
 
+#Second Button and url
 button2 = digitalio.DigitalInOut(board.D23)
 button2.direction = digitalio.Direction.INPUT
 button2.pull = digitalio.Pull.UP
 
+#Third Button and url
 button3 = digitalio.DigitalInOut(board.D5)
 button3.direction = digitalio.Direction.INPUT
 button3.pull = digitalio.Pull.UP
 
+#Fourth Button and url
 button4 = digitalio.DigitalInOut(board.D17)
 button4.direction = digitalio.Direction.INPUT
 button4.pull = digitalio.Pull.UP
 
+#Fifth button and url
 button5 = digitalio.DigitalInOut(board.D22)
 button5.direction = digitalio.Direction.INPUT
 button5.pull = digitalio.Pull.UP
 
+#A poor mans pause button in case of awful sound
 button6 = digitalio.DigitalInOut(board.D7)
 button6.direction = digitalio.Direction.INPUT
 button6.pull = digitalio.Pull.UP
 
-
+#A shutdown button
+button = digitalio.DigitalInOut(board.D3)
+button7.direction = digitalio.Direction.INPUT
+button7.pull = digitalio.Pull.UP
 
 while True:
 
-        # omxplayer -o local <file>
-        # omxplayer -o hdmi <file>
-        # omxplayer -o both <file>
      if not button1.value:
         subprocess.call(['killall', 'mpg123'])
         print("stopped previous track")
@@ -52,6 +60,7 @@ while True:
         subprocess.call(['killall', 'mpg123'])
         os.system('mpg123 -q http://server1.chilltrax.com:9000/ &')
         print ("playing chilltrax")
+        
      if not button3.value:
         subprocess.call(['killall', 'mpg123'])
         print("stopped previous track")
@@ -70,9 +79,12 @@ while True:
         os.system('mpg123 -q http://ice1.somafm.com/groovesalad-256-mp3 &')
         print ("playing groove salad")
 
-     if not button6.value:
+     if not button6.value:  #stop playing music
         subprocess.call(['killall', 'mpg123'])
         print("music paused - press button to continue")
+        
+     if not button7.value:  #put the pi to sleep
+        subprocess.call(['shutdown', '-h', 'now'], shell=False)  
 
 time.sleep(.25)
 
